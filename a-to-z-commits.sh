dff4134cd4d37e41e0522b05d5f9daaf960b4d0a
Othmane Mimouni <your_verified_email@example.com>
2025-05-05 23:49:02 +0000

    fafa

---
+++ a-to-z-commits.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+mkdir github-a-to-z
+cd github-a-to-z
+git init
+
+# Set your GitHub user info
+git config user.name "Othmane Mimouni"
+git config user.email "utrshall@gmail.com"  # Use a verified GitHub email
+
+# Letters A to Z and years
+letters=( {A..Z} )
+years=( {2000..2025} )
+
+# Loop A-Z
+for i in "${!letters[@]}"; do
+  letter=${letters[$i]}
+  year=${years[$i]:-2025}
+
+  echo "$letter" > file.txt
+  git add file.txt
+
+  commit_date="$year-01-01T12:00:00"
+  GIT_COMMITTER_DATE="$commit_date" git commit --date="$commit_date" -m "Commit $letter"
+done
